cb5f6860b2c1d992dcf0d9c845d6cba7ee604707
Paul Loh <lohpaul9@gmail.com>
2024-12-17 17:28:37 -0500

    paths fixed

--- webapp-sketcher/main.py
+++ webapp-sketcher/main.py
@@ -6,12 +6,14 @@ import os
 import json
 
 app = FastAPI()
+data_dir = "../data/"
+image_dir = data_dir + "/annotation_images"
 
 # Serve static files (HTML, JS, images)
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 # Load the mapping file
-with open('../data/PIE-Bench_v1/mapping_file.json', 'r') as file:
+with open(f'{data_dir}/mapping_file.json', 'r') as file:
     mapping_data = json.load(file)
 
 # Route to serve the sketching page
@@ -31,7 +33,7 @@ async def get_images():
 # New route to serve images
 @app.get("/images/{image_path:path}")
 async def serve_image(image_path: str):
-    full_image_path = f"../data/PIE-Bench_v1/annotation_images/{image_path}"
+    full_image_path = f"{image_dir}/{image_path}"
     print("Constructed image path: ", full_image_path)  # Log the constructed path
     if not os.path.exists(full_image_path):
         print("Image not found: ", full_image_path)  # Log if the image does not exist
@@ -43,7 +45,7 @@ async def serve_image(image_path: str):
 @app.post("/save/")
 async def save_image(file: UploadFile = File(...), image_path: str = ""):
     filename = image_path.split('.')[0]  # Get the filename without extension
-    output_filename = f"../data/PIE-Bench_v1/annotation_images/{filename}_sketch.png"  # Save in the same directory
+    output_filename = f"{image_dir}/{filename}_sketch.png"  # Save in the same directory
     print("Saving image to: ", output_filename)
     with open(output_filename, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
